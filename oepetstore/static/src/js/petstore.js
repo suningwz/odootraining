c69f112451be44dde6da31f4125cdffba089df8a
suningwz <2437526685@qq.com>
2019-04-21 08:43:50 +0800

    更新代码到新版本

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -1,4 +1,4 @@
-openerp.oepetstore = function(instance, local) {
+odoo.oepetstore = function(instance, local) {
     var _t = instance.web._t,
         _lt = instance.web._lt;
     var QWeb = instance.web.qweb;

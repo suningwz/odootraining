f5bcd2997faf312ef42a797c05cfc8a77c9b70d2
suningwz <2437526685@qq.com>
2019-04-23 07:06:11 +0800

    oepetstore可以正常运行，在谷歌浏览器控制台可以看到console.log输出信息

--- oepetstore/static/src/js/petstore.js
+++ oepetstore/static/src/js/petstore.js
@@ -1,12 +1,17 @@
+//首先通过odoo.define("namespaceName",function(require){});声明当前作用域适用范围
 odoo.define('oepetstore.petstore', function (require) {
 "use strict";
+    //通过require引入要使用的js模块
     var Class = require('web.Class');
     var Widget = require('web.Widget');
     var core = require('web.core');
     var utils = require('web.utils');
     var _t = core._t;
     var _lt = core._lt;
-
+    
+    //创建Widget  
+    //主要属性有className--widget创建的dom元素的class，events--widget的事件表，temlate--widget的qweb模板
+    //主要方法有init--初始化实例的方法，start--widget自动调用的方法
     var homePage = Widget.extend({
         init: function(parent) {
             this._super(parent);
@@ -17,5 +22,6 @@ odoo.define('oepetstore.petstore', function (require) {
         },
     });
     core.action_registry.add('petstore', homePage);
+    return homePage;
 
 });
